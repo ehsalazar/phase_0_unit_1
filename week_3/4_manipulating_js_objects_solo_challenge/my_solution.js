78df4e90dfc80565a05cded016c82c5181fe718a
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-06 18:09:45 -0700

    Edit my solution for Manipulating JavaScript Objects

--- week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
+++ week_3/4_manipulating_js_objects_solo_challenge/my_solution.js
@@ -12,33 +12,65 @@ var terah = {
   hairColor: "brown",
   eyeColor: "brown"
 }
+
 /* Pseudocode Section - write pseudocode for each challenge below.
 1. Define a variable adam and use object literal notation to assign this variable 
    the value of a JavaScript Object object with no properties.
 
+   - Can be created using var object = {};
+
 2. Give adam a name property with the value "Adam".
+
+  - Use dot notation to add the value "Adam" to the name property. 
+  - object.property = value
 
 3. Add a spouse property to terah and assign it the value of adam.
 
+  - Use dot notation to add the object adam to the spouse property of the object terah.
+  - object.property = other object
+
 4. Change the value of the terah weight property to 125.
+
+  -Use dot notation to change the value of the weight property to 125.
+  - object.property = new value
 
 5. Remove the eyeColor property from terah.
 
+  - Delete the property from the object.
+  - delete object.property
+
 6. Add a spouse property to adam and assign it the value of terah.
 
-7. Add a children property to terah and and use object literal notation to assign 
+  - Similar to step 3. object.property = other object
+
+7. Add a children property to terah and and use object literal notation to assign
    this variable the value of a JavaScript Object object with no properties
+
+  - Use both dot notation and literal notation to complete.
+  - object.property = {}; (a new empty object within the parent object)
 
 8. Add a carson property to the value of the terah children property and assign it 
   the value of an object with the property name with a value of "Carson".
 
+  - Use dot notation to build.
+  - parentObject.parentProperty.newChildPropery = {newSubChildProperty: value};
+
 9. Add a carter property to the value of the terah children property and assign it 
    the value of an object with the property name with a value of "Carter".
+
+  - Similar to step 8.
+  - parentObject.parentProperty.newChildPropery = {newSubChildProperty: value};
 
 10. Add a colton property to the value of the terah children property and assign it 
     the value of an object with the property name with a value of "Colton".
 
+  - Similar to step 8.
+  - parentObject.parentProperty.newChildPropery = {newSubChildProperty: value};
+
 11. Add a children property to adam and assign it the value of terah children.
+
+  - Use dot notation to complete.
+  - object.property = object.proper
 
 
 */
@@ -46,24 +78,33 @@ var terah = {
 // __________________________________________
 // Write your code below.
 
-
-
-
-
-
-
+var adam = {};
+adam.name = "Adam";
+terah.spouse = adam;
+terah.weight = 125;
+delete terah.eyeColor;
+adam.spouse = terah;
+terah.children = {};
+terah.children.carson = {name: "Carson"};
+terah.children.carter = {name: "Carter"};
+terah.children.colton = {name: "Colton"};
+adam.children = terah.children;
 
 // __________________________________________
 // Reflection: Use the reflection guidelines
-// 
-// 
-// 
-// 
-// 
-// 
 
+// Having completed the previous challenge successfully with peers made this challenge make much more sense. 
+// In other words, being familiar with the tools made it easier to work with them.  
 
-// __________________________________________
+// The only concept I'm having to work through is that of pseudocode. To me, reading the outline provided
+// already was the pseudocode. I tried to make it more meaningful by breaking down the concepts into
+// keywords for each step. I'm not sure however if that would be considered good pseudocode.
+
+// I feel confident in the challenges Learning Objectives overall. I again enjoyed this challenge with it's 
+// riddle nature. It made the process of overcoming each error more meaningful and had me putting more thought 
+// into the code process. I didn't find any aspect of this challenge tedious or to overwhelming to overcome.
+
+// // __________________________________________
 // Driver Code:  Do not alter code below this line.
 function assert(test, message, test_number) {
   if (!test) {
