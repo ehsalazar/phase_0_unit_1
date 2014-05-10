3ebdaa1dcdc5c7e12430a68bf85d94decb27bab1
Ernie Salazar <ehsalazar75@gmail.com>
2014-05-09 22:28:38 -0700

    Spacing

--- week_3/6_group_project/gc1_pseudocode.js
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -1,28 +1,30 @@
 // U1.W3.6 Group Project - Pseudocode
 
 
-variable sum is a function (with a parameter of an array){
-    variable total is set to zero;
-	for (index starts at 0, until you reach the length of an array, increment index by one) {
-	total is equal to total plus the array[index];
-	};
-	return the total;
-};
+// 	variable sum is a function (with a parameter of an array){
+// 		variable total is set to zero;
+// 		for (index starts at 0, until you reach the length of an array, increment index by one) {
+// 		total is equal to total plus the array[index];
+// 		};
+// 		return the total;
+// 	};
 
-variable mean is a function (with a parameter of an array){
-   	variable total is set to zero;
-	for (index starts at 0, until you reach the length of an array, increment index by one) {
-	total is equal to total plus the array[index];
-	};
-	total is equal to total divided by the length of an array;
-	return the total;
-};
+// 	variable mean is a function (with a parameter of an array){
+// 		variable total is set to zero;
+// 		for (index starts at 0, until you reach the length of an array, increment index by one) {
+// 		total is equal to total plus the array[index];
+// 		};
+// 		total is equal to total divided by the length of an array;
+// 		return the total;
+// 	};
 
-variable median is a function (with a parameter of an array){
-   	Use dot notation to sort the array with ( a function(high, low) {return high - low})
-   	variable midpoint is equal to Math.floor(length of array divided by 2);
-   	if ((length of array modulus 2) is equal to 1)
-    	return the array[midpoint];
-   	else
-    	return the ((array[midpoint minus 1] plus array[midpoint]) divided by 2);
-};
+// 	variable median is a function (with a parameter of an array){
+//  	Use dot notation to sort the array with ( a function(high, low) {return high - low})
+//   	variable midpoint is equal to Math.floor(length of array divided by 2);
+//   	if ((length of array modulus 2) is equal to 1)
+//     		return the array[midpoint];
+//   	else
+//     		return the ((array[midpoint minus 1] plus array[midpoint]) divided by 2);
+// 	};
+
+
